f7b734f6d63afbf148a55785895fb0d1c4153ed1
Ihtisham Mehmood <ahtasham.marral12@gmail.com>
2025-04-07 16:16:21 +0500

    Embedding model and LLM updated

--- utils.py
+++ utils.py
@@ -53,7 +53,7 @@ def display_msg(msg, author):
     st.chat_message(author).write(msg)
 
 def configure_llm():
-    available_llms = ["deepseek-r1", "llama-3.3:70b-versatile"]
+    available_llms = ["deepseek-r1", "llama-scount-17b"]
     llm_opt = st.sidebar.radio(
         label="LLM",
         options=available_llms,
@@ -67,11 +67,11 @@ def configure_llm():
             model_name="deepseek-r1-distill-llama-70b",
             streaming=True
         )
-    elif llm_opt == "llama-3.3:70b-versatile":
+    elif llm_opt == "llama-scount-17b":
         llm = ChatGroq(
             temperature=0.7,
             groq_api_key=groq_api_key,
-            model_name="llama-3.3-70b-versatile",
+            model_name="meta-llama/llama-4-scout-17b-16e-instruct",
             streaming=True
         )
     return llm
@@ -83,7 +83,7 @@ def print_qa(cls, question, answer):
 @st.cache_resource
 def configure_embedding_model():
     embedding_model = HuggingFaceEmbeddings(
-            model_name="sentence-transformers/all-MiniLM-L6-v2",
+            model_name="BAAI/bge-small-en-v1.5",
             model_kwargs={'device': 'cpu'},
             encode_kwargs={'normalize_embeddings': False}
         )
