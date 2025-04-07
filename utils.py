b4a42855adea475faa39be393b309f7473c1e2f2
Ihtisham Mehmood <ahtasham.marral12@gmail.com>
2025-04-07 16:27:22 +0500

    Requirements.txt file updated and embedding model reverted

--- utils.py
+++ utils.py
@@ -83,7 +83,7 @@ def print_qa(cls, question, answer):
 @st.cache_resource
 def configure_embedding_model():
     embedding_model = HuggingFaceEmbeddings(
-            model_name="BAAI/bge-small-en-v1.5",
+            model_name="sentence-transformers/all-MiniLM-L6-v2",
             model_kwargs={'device': 'cpu'},
             encode_kwargs={'normalize_embeddings': False}
         )
