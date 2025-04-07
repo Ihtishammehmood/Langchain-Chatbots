d632a83080018f42e6247616bed42f54aa7b07d7
Ihtisham Mehmood <ahtasham.marral12@gmail.com>
2025-04-07 17:13:43 +0500

    Revert "Hugging_face issue resolved"
    This reverts commit 595da92a6136a9028746c82d213fc80f7e7aefcc.

--- utils.py
+++ utils.py
@@ -3,9 +3,7 @@ import streamlit as st
 from datetime import datetime
 from streamlit.logger import get_logger
 from langchain_groq import ChatGroq
-# from langchain.embeddings import HuggingFaceEmbeddings
-from langchain_huggingface import HuggingFaceEmbeddings
-
+from langchain.embeddings import HuggingFaceEmbeddings
 
 
 
